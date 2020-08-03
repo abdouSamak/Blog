fbab60de1d04cbc4c8dc4f3cd3bf424a55d46129
abdou <abdou.samake0908@gmail.com>
2020-08-03 23:20:13 +0200

    fix abdou blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AccueilComponent} from "./components/accueil/accueil.component";
+import {CompetencesComponent} from "./components/competences/competences.component";
+import {PortfolioComponent} from "./components/portfolio/portfolio.component";
+import {LoisirsComponent} from "./components/loisirs/loisirs.component";
+import {ContactsComponent} from "./components/contacts/contacts.component";
+import {ExperiencesComponent} from "./components/experiences/experiences.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'experiences', component: ExperiencesComponent},
+  {path: 'competences', component: CompetencesComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'loisirs', component: LoisirsComponent},
+  {path: 'contacts', component: ContactsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { CompetencesComponent } from './components/competences/competences.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { LoisirsComponent } from './components/loisirs/loisirs.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    CompetencesComponent,
+    ContactsComponent,
+    ExperiencesComponent,
+    LoisirsComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
